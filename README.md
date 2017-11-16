21ab880ee5ceb4f527e538ed7d76cb58dbdf8f46
Stefano Savarino <stefano@reachconsulting.se>
2017-11-16 21:55:57 +0100

    sudoku_solver.cpp created. .gitignore added.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cplusplus_stuff
 Smaller c++ projects
+
+20171116: sudoku_solver.cpp
---
+++ sudoku_solver.cpp
@@ -0,0 +1,9 @@
+// sudoku_solver.cpp
+
+#include <iostream>
+
+int main(void) {
+	std::cout << "Sudoku Solver v0.1" << std::endl;
+	
+	return 0;
+}
